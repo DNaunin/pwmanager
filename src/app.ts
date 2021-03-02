ab7f5c07014a91a5a449273fa28947ecedb6a7eb
DNaunin <d.naunin@gmail.com>
2021-03-02 09:54:16 +0100

    add colors with chalk

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,35 @@
-const [command] = process.argv.slice(2);
+import prompts from "prompts";
+import chalk from "chalk";
 
-if (command === "set") {
-  console.log("You like set?");
-} else if (command === "get") {
-  console.log("You like to get cake?");
-}
+const prompts = require("prompts");
+
+const questions = [
+  {
+    type: "text",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "number",
+    name: "age",
+    message: "How old are you?",
+  },
+  {
+    type: "text",
+    name: "about",
+    message: chalk.red("What is the answer to everything?"),
+    initial: "42",
+    validate: (value: string) =>
+      value === "42" ? chalk.blue(`You are so wise!`) : `Wrong`,
+  },
+];
+
+const run = async () => {
+  console.log(
+    chalk.blue("Welcome ") + chalk.green("to the Password Manager 🔐")
+  );
+
+  const response = await prompts(questions);
+};
+
+run();
